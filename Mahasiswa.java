e147a7ec005dd95fbee47156a6210987457555a4
renz041 <163615605+renz041@users.noreply.github.com>
2025-10-06 11:17:25 +0700

    Create Mahasiswa.java

---
+++ Mahasiswa.java
@@ -0,0 +1,12 @@
+public class Mahasiswa {
+    private String nama;
+    private String nrp;
+
+    public Mahasiswa(String nama, String nrp) {
+        this.nama = nama;
+        this.nrp = nrp;
+    }
+
+    public String getNama() { return nama; }
+    public String getNrp() { return nrp; }
+}
